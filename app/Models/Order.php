557ae6ddea189291ee17543785622506657b6810
apple <denis.rozganyaev@gmail.com>
2024-08-27 21:23:53 +0300

    websockets

--- app/Models/Order.php
+++ app/Models/Order.php
@@ -7,10 +7,11 @@ use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
 use Illuminate\Database\Eloquent\Relations\BelongsToMany;
 use Illuminate\Database\Eloquent\Relations\HasOne;
+use Kyslik\ColumnSortable\Sortable;
 
 class Order extends Model
 {
-    use HasFactory;
+    use HasFactory, Sortable;
 
     protected $guarded = [];
 
