d213cb34fff290c2df4868a1d6df8e988dc4073c
apple <denis.rozganyaev@gmail.com>
2024-08-30 20:38:48 +0300

    test

--- resources/views/mail/new-price-mail.blade.php
+++ resources/views/mail/new-price-mail.blade.php
@@ -4,14 +4,14 @@
 The body of your message.
 
 <x-mail::button :url="$url">
-Button Text
+Button Text 2
 </x-mail::button>
 
 
 
 
 
-    
+
 Thanks,<br>
 {{ config('app.name') }}
 </x-mail::message>
