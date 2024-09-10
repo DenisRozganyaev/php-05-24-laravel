2e284ebc4f1d6d5214096e71077e5614f1178f3e
apple <denis.rozganyaev@gmail.com>
2024-09-10 20:28:49 +0300

    updated

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -10,11 +10,20 @@
             </div>
         </div>
     </div>
+
+
+
+
     <div class="row justify-content-center">
         <div class="col-12 py-5">
             <h1>Latest products</h1>
         </div>
     </div>
+
+
+
+
+    
     <div class="row row-cols-1 row-cols-sm-1 row-cols-md-3 row-cols-lg-4 g-3">
         @each('products.parts.card', $products, 'product')
     </div>
